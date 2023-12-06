1f9c1e3875a9b26e424f4ba02d6bdb550319de7a
Ivan Zakatov <vanchesboy@gmail.com>
2023-12-06 13:50:24 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] {"111", "GG", "Artem", ":-)", "Russia"};
+string[] array2 = new string[array1.Length];
+
+void CreateArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+CreateArray(array1, array2);
+PrintArray(array2);
